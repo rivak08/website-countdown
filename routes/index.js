5b552c021dd03b11ce5128037fc36bf391c1d864
rivak08 <Thekajangu@gmail.com>
2023-02-09 15:49:53 -0500

    Add my changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime= new Date('2/10/2023 10:00').getTime()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime()
+  const numberOfMilliseconds = parseInt(launchTime - currentTime)
+  res.render(
+    'index', 
+    { title: 'My greatnest Game', countDown: numberOfMilliseconds 
+  });
 });
 
 module.exports = router;
